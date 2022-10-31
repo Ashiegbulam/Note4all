a474207d6bb6a3595fdc93ce1da4d42e7dd7f226
Ashiegbulam <ashiegbulam@icloud.com>
2022-10-31 04:55:35 +0100

    settings.py

--- Note4all/settings.py
+++ Note4all/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -130,8 +131,8 @@ LOGIN_URL = '/users/login/'
 BOOTSTRAP3 = {
     'include_jquery': True,
 }
+
 # Heroku settings
-import os
 if os.getcwd() == '/app':
     import dj_database_url
     DATABASES = {
